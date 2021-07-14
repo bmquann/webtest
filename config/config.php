1a2b8450862c8e14801831940e7337046b208ecc
Quan <manhquan24072000@gmail.com>
2021-07-14 13:44:04 +0700

    first commit

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST", '52.6.114.59');
-define("DB_USER", 'quan');
-define("DB_PASS", '123@123a');
-define("DB_NAME", 'ATN');
+define("DB_HOST", 'localhost');
+define("DB_USER", 'root');
+define("DB_PASS", '');
+define("DB_NAME", 'web_mvcphu');
 ?>
